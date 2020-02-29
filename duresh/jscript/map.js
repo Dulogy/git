2bb84fa91d72bdd45a0e80dd6dc3641abf04ca14
durgesh <kumardurgesh581@gmail.com>
2020-02-29 08:40:30 +0530

    Adding Durgesh File

--- duresh/jscript/map.js
+++
@@ -1,8 +0,0 @@
-
-let a={
-    name:"durgesh",
-    id:[1,2]
-}
-let map=new Map(Object.entries(a));
-//console.log(map.get(1));
-console.log(map.get('name'));
--- duresh/jscript/timeout1.js
+++
@@ -1,9 +0,0 @@
-
-for(i=0;i<10;i++)
-{
-function sayHi() {
-    console.log(i);
-  }
-  
-  setTimeout(sayHi, 1000+100*i);
-}
